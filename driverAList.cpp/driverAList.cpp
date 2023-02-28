9f07717687073247480114f035547629b4c0236d
Carlos Burbano <burbanocarlos1000@gmail.com>
2023-02-28 17:20:16 -0600

    coimment out

--- driverAList.cpp/driverAList.cpp
+++ driverAList.cpp/driverAList.cpp
@@ -44,326 +44,326 @@ int main()
         cout << "List is full" << endl;
     }
     
-    if (shortList.insertFront(3))
-    {
-        shortList.display();
-        shortList.getSmallest(min);
-        
-        cout << endl << "capacity is " << shortList.getCapacity() << "\t numValues is "
-        << shortList.getNumValues() << "\t smallest value is " << min << endl;
-    }
-    else
-    {
-        cout << "Unable to insert the value 3";
-    }
-    
-    if (shortList.insertBack(3))
-    {
-        shortList.display();
-        shortList.getSmallest(min);
-        
-        cout << endl << "capacity is " << shortList.getCapacity() << "\t numValues is "
-        << shortList.getNumValues() << "\t smallest value is " << min << endl;
-    }
-    else
-    {
-        cout << "Unable to insert the value 3";
-    }
-    
-    if (shortList.isEmpty())
-    {
-        cout << "List is empty" << endl;
-    }
-    else if (shortList.isFull())
-    {
-        cout << "List is full" << endl;
-    }
-    
-    if (shortList.insertAtIndex(5,1))
-    {
-        shortList.display();
-        shortList.getSmallest(min);
-        
-        cout << endl << "capacity is " << shortList.getCapacity() << "\t numValues is "
-        << shortList.getNumValues() << "\t smallest value is " << min << endl;
-    }
-    else
-    {
-        cout << "Unable to insert the value 5 at index 1";
-    }
-    
-    if (shortList.isEmpty())
-    {
-        cout << "List is empty" << endl;
-    }
-    else if (shortList.isFull())
-    {
-        cout << "List is full" << endl;
-    }
-    
-    if (shortList.removeFront(dataOut))
-    {
-        shortList.display();
-        shortList.getSmallest(min);
-        
-        cout << endl << "capacity is " << shortList.getCapacity() << "\t numValues is "
-        << shortList.getNumValues() << "\t smallest value is " << min << endl;
-    }
-    else
-    {
-        cout << "Unable to insert the value 5 at index 1";
-    }
-    
-    if (shortList.isEmpty())
-    {
-        cout << "List is empty" << endl;
-    }
-    else if (shortList.isFull())
-    {
-        cout << "List is full" << endl;
-    }
-    
-    if (shortList.removeBack(dataOut))
-    {
-        shortList.display();
-        shortList.getSmallest(min);
-        
-        cout << endl << "capacity is " << shortList.getCapacity() << "\t numValues is "
-        << shortList.getNumValues() << "\t smallest value is " << min << endl;
-    }
-    else
-    {
-        cout << "Unable to insert the value 5 at index 1";
-    }
-    
-    if (shortList.isEmpty())
-    {
-        cout << "List is empty" << endl;
-    }
-    else if (shortList.isFull())
-    {
-        cout << "List is full" << endl;
-    }
-    
-    if (shortList.removeAtIndex(dataOut,0))
-    {
-        shortList.display();
-        shortList.getSmallest(min);
-        
-        cout << endl << "capacity is " << shortList.getCapacity() << "\t numValues is "
-        << shortList.getNumValues() << "\t smallest value is " << min << endl;
-    }
-    else
-    {
-        cout << "Unable to insert the value 5 at index 1";
-    }
-    
-    if (shortList.isEmpty())
-    {
-        cout << "List is empty" << endl;
-    }
-    else if (shortList.isFull())
-    {
-        cout << "List is full" << endl;
-    }
-    
-    if (shortList.remove(dataOut))
-    {
-        shortList.display();
-        shortList.getSmallest(min);
-        
-        cout << endl << "capacity is " << shortList.getCapacity() << "\t numValues is "
-        << shortList.getNumValues() << "\t smallest value is " << min << endl;
-    }
-    else
-    {
-        cout << "Unable to insert the value 5 at index 1";
-    }
-    
-    if (shortList.isEmpty())
-    {
-        cout << "List is empty" << endl;
-    }
-    else if (shortList.isFull())
-    {
-        cout << "List is full" << endl;
-    }
-    
-    if (shortList.retrieveFront(dataOut))
-    {
-        shortList.display();
-        shortList.getSmallest(min);
-        
-        cout << endl << "capacity is " << shortList.getCapacity() << "\t numValues is "
-        << shortList.getNumValues() << "\t smallest value is " << min << endl;
-    }
-    else
-    {
-        cout << "Unable to insert the value 5 at index 1";
-    }
-    
-    if (shortList.isEmpty())
-    {
-        cout << "List is empty" << endl;
-    }
-    else if (shortList.isFull())
-    {
-        cout << "List is full" << endl;
-    }
-    
-    if (shortList.retrieveBack(dataOut))
-    {
-        shortList.display();
-        shortList.getSmallest(min);
-        
-        cout << endl << "capacity is " << shortList.getCapacity() << "\t numValues is "
-        << shortList.getNumValues() << "\t smallest value is " << min << endl;
-    }
-    else
-    {
-        cout << "Unable to insert the value 5 at index 1";
-    }
-    
-    if (shortList.isEmpty())
-    {
-        cout << "List is empty" << endl;
-    }
-    else if (shortList.isFull())
-    {
-        cout << "List is full" << endl;
-    }
-    
-    if (shortList.retrieveAtIndex(dataOut,3))
-    {
-        shortList.display();
-        shortList.getSmallest(min);
-        
-        cout << endl << "capacity is " << shortList.getCapacity() << "\t numValues is "
-        << shortList.getNumValues() << "\t smallest value is " << min << endl;
-    }
-    else
-    {
-        cout << "Unable to insert the value 5 at index 1";
-    }
-    
-    if (shortList.isEmpty())
-    {
-        cout << "List is empty" << endl;
-    }
-    else if (shortList.isFull())
-    {
-        cout << "List is full" << endl;
-    }
-    
-    if (shortList.retrieve(dataOut))
-    {
-        shortList.display();
-        shortList.getSmallest(min);
-        
-        cout << endl << "capacity is " << shortList.getCapacity() << "\t numValues is "
-        << shortList.getNumValues() << "\t smallest value is " << min << endl;
-    }
-    else
-    {
-        cout << "Unable to insert the value 5 at index 1";
-    }
-    
-    if (shortList.isEmpty())
-    {
-        cout << "List is empty" << endl;
-    }
-    else if (shortList.isFull())
-    {
-        cout << "List is full" << endl;
-    }
-    
-    if (shortList.updateFront(3))
-    {
-        shortList.display();
-        shortList.getSmallest(min);
-        
-        cout << endl << "capacity is " << shortList.getCapacity() << "\t numValues is "
-        << shortList.getNumValues() << "\t smallest value is " << min << endl;
-    }
-    else
-    {
-        cout << "Unable to insert the value 5 at index 1";
-    }
-    
-    if (shortList.isEmpty())
-    {
-        cout << "List is empty" << endl;
-    }
-    else if (shortList.isFull())
-    {
-        cout << "List is full" << endl;
-    }
-    
-    if (shortList.updateBack(3))
-    {
-        shortList.display();
-        shortList.getSmallest(min);
-        
-        cout << endl << "capacity is " << shortList.getCapacity() << "\t numValues is "
-        << shortList.getNumValues() << "\t smallest value is " << min << endl;
-    }
-    else
-    {
-        cout << "Unable to insert the value 5 at index 1";
-    }
-    
-    if (shortList.isEmpty())
-    {
-        cout << "List is empty" << endl;
-    }
-    else if (shortList.isFull())
-    {
-        cout << "List is full" << endl;
-    }
-    
-    if (shortList.updateAtIndex(dataIn,3))
-    {
-        shortList.display();
-        shortList.getSmallest(min);
-        
-        cout << endl << "capacity is " << shortList.getCapacity() << "\t numValues is "
-        << shortList.getNumValues() << "\t smallest value is " << min << endl;
-    }
-    else
-    {
-        cout << "Unable to insert the value 5 at index 1";
-    }
-    
-    if (shortList.isEmpty())
-    {
-        cout << "List is empty" << endl;
-    }
-    else if (shortList.isFull())
-    {
-        cout << "List is full" << endl;
-    }
-    
-    if (shortList.update(dataIn))
-    {
-        shortList.display();
-        shortList.getSmallest(min);
-        
-        cout << endl << "capacity is " << shortList.getCapacity() << "\t numValues is "
-        << shortList.getNumValues() << "\t smallest value is " << min << endl;
-    }
-    else
-    {
-        cout << "Unable to insert the value 5 at index 1";
-    }
-    
-    if (shortList.isEmpty())
-    {
-        cout << "List is empty" << endl;
-    }
-    else if (shortList.isFull())
-    {
-        cout << "List is full" << endl;
-    }
+//    if (shortList.insertFront(3))
+//    {
+//        shortList.display();
+//        shortList.getSmallest(min);
+//        
+//        cout << endl << "capacity is " << shortList.getCapacity() << "\t numValues is "
+//        << shortList.getNumValues() << "\t smallest value is " << min << endl;
+//    }
+//    else
+//    {
+//        cout << "Unable to insert the value 3";
+//    }
+//    
+//    if (shortList.insertBack(3))
+//    {
+//        shortList.display();
+//        shortList.getSmallest(min);
+//        
+//        cout << endl << "capacity is " << shortList.getCapacity() << "\t numValues is "
+//        << shortList.getNumValues() << "\t smallest value is " << min << endl;
+//    }
+//    else
+//    {
+//        cout << "Unable to insert the value 3";
+//    }
+//    
+//    if (shortList.isEmpty())
+//    {
+//        cout << "List is empty" << endl;
+//    }
+//    else if (shortList.isFull())
+//    {
+//        cout << "List is full" << endl;
+//    }
+//    
+//    if (shortList.insertAtIndex(5,1))
+//    {
+//        shortList.display();
+//        shortList.getSmallest(min);
+//        
+//        cout << endl << "capacity is " << shortList.getCapacity() << "\t numValues is "
+//        << shortList.getNumValues() << "\t smallest value is " << min << endl;
+//    }
+//    else
+//    {
+//        cout << "Unable to insert the value 5 at index 1";
+//    }
+//    
+//    if (shortList.isEmpty())
+//    {
+//        cout << "List is empty" << endl;
+//    }
+//    else if (shortList.isFull())
+//    {
+//        cout << "List is full" << endl;
+//    }
+//    
+//    if (shortList.removeFront(dataOut))
+//    {
+//        shortList.display();
+//        shortList.getSmallest(min);
+//        
+//        cout << endl << "capacity is " << shortList.getCapacity() << "\t numValues is "
+//        << shortList.getNumValues() << "\t smallest value is " << min << endl;
+//    }
+//    else
+//    {
+//        cout << "Unable to insert the value 5 at index 1";
+//    }
+//    
+//    if (shortList.isEmpty())
+//    {
+//        cout << "List is empty" << endl;
+//    }
+//    else if (shortList.isFull())
+//    {
+//        cout << "List is full" << endl;
+//    }
+//    
+//    if (shortList.removeBack(dataOut))
+//    {
+//        shortList.display();
+//        shortList.getSmallest(min);
+//        
+//        cout << endl << "capacity is " << shortList.getCapacity() << "\t numValues is "
+//        << shortList.getNumValues() << "\t smallest value is " << min << endl;
+//    }
+//    else
+//    {
+//        cout << "Unable to insert the value 5 at index 1";
+//    }
+//    
+//    if (shortList.isEmpty())
+//    {
+//        cout << "List is empty" << endl;
+//    }
+//    else if (shortList.isFull())
+//    {
+//        cout << "List is full" << endl;
+//    }
+//    
+//    if (shortList.removeAtIndex(dataOut,0))
+//    {
+//        shortList.display();
+//        shortList.getSmallest(min);
+//        
+//        cout << endl << "capacity is " << shortList.getCapacity() << "\t numValues is "
+//        << shortList.getNumValues() << "\t smallest value is " << min << endl;
+//    }
+//    else
+//    {
+//        cout << "Unable to insert the value 5 at index 1";
+//    }
+//    
+//    if (shortList.isEmpty())
+//    {
+//        cout << "List is empty" << endl;
+//    }
+//    else if (shortList.isFull())
+//    {
+//        cout << "List is full" << endl;
+//    }
+//    
+//    if (shortList.remove(dataOut))
+//    {
+//        shortList.display();
+//        shortList.getSmallest(min);
+//        
+//        cout << endl << "capacity is " << shortList.getCapacity() << "\t numValues is "
+//        << shortList.getNumValues() << "\t smallest value is " << min << endl;
+//    }
+//    else
+//    {
+//        cout << "Unable to insert the value 5 at index 1";
+//    }
+//    
+//    if (shortList.isEmpty())
+//    {
+//        cout << "List is empty" << endl;
+//    }
+//    else if (shortList.isFull())
+//    {
+//        cout << "List is full" << endl;
+//    }
+//    
+//    if (shortList.retrieveFront(dataOut))
+//    {
+//        shortList.display();
+//        shortList.getSmallest(min);
+//        
+//        cout << endl << "capacity is " << shortList.getCapacity() << "\t numValues is "
+//        << shortList.getNumValues() << "\t smallest value is " << min << endl;
+//    }
+//    else
+//    {
+//        cout << "Unable to insert the value 5 at index 1";
+//    }
+//    
+//    if (shortList.isEmpty())
+//    {
+//        cout << "List is empty" << endl;
+//    }
+//    else if (shortList.isFull())
+//    {
+//        cout << "List is full" << endl;
+//    }
+//    
+//    if (shortList.retrieveBack(dataOut))
+//    {
+//        shortList.display();
+//        shortList.getSmallest(min);
+//        
+//        cout << endl << "capacity is " << shortList.getCapacity() << "\t numValues is "
+//        << shortList.getNumValues() << "\t smallest value is " << min << endl;
+//    }
+//    else
+//    {
+//        cout << "Unable to insert the value 5 at index 1";
+//    }
+//    
+//    if (shortList.isEmpty())
+//    {
+//        cout << "List is empty" << endl;
+//    }
+//    else if (shortList.isFull())
+//    {
+//        cout << "List is full" << endl;
+//    }
+//    
+//    if (shortList.retrieveAtIndex(dataOut,3))
+//    {
+//        shortList.display();
+//        shortList.getSmallest(min);
+//        
+//        cout << endl << "capacity is " << shortList.getCapacity() << "\t numValues is "
+//        << shortList.getNumValues() << "\t smallest value is " << min << endl;
+//    }
+//    else
+//    {
+//        cout << "Unable to insert the value 5 at index 1";
+//    }
+//    
+//    if (shortList.isEmpty())
+//    {
+//        cout << "List is empty" << endl;
+//    }
+//    else if (shortList.isFull())
+//    {
+//        cout << "List is full" << endl;
+//    }
+//    
+//    if (shortList.retrieve(dataOut))
+//    {
+//        shortList.display();
+//        shortList.getSmallest(min);
+//        
+//        cout << endl << "capacity is " << shortList.getCapacity() << "\t numValues is "
+//        << shortList.getNumValues() << "\t smallest value is " << min << endl;
+//    }
+//    else
+//    {
+//        cout << "Unable to insert the value 5 at index 1";
+//    }
+//    
+//    if (shortList.isEmpty())
+//    {
+//        cout << "List is empty" << endl;
+//    }
+//    else if (shortList.isFull())
+//    {
+//        cout << "List is full" << endl;
+//    }
+//    
+//    if (shortList.updateFront(3))
+//    {
+//        shortList.display();
+//        shortList.getSmallest(min);
+//        
+//        cout << endl << "capacity is " << shortList.getCapacity() << "\t numValues is "
+//        << shortList.getNumValues() << "\t smallest value is " << min << endl;
+//    }
+//    else
+//    {
+//        cout << "Unable to insert the value 5 at index 1";
+//    }
+//    
+//    if (shortList.isEmpty())
+//    {
+//        cout << "List is empty" << endl;
+//    }
+//    else if (shortList.isFull())
+//    {
+//        cout << "List is full" << endl;
+//    }
+//    
+//    if (shortList.updateBack(3))
+//    {
+//        shortList.display();
+//        shortList.getSmallest(min);
+//        
+//        cout << endl << "capacity is " << shortList.getCapacity() << "\t numValues is "
+//        << shortList.getNumValues() << "\t smallest value is " << min << endl;
+//    }
+//    else
+//    {
+//        cout << "Unable to insert the value 5 at index 1";
+//    }
+//    
+//    if (shortList.isEmpty())
+//    {
+//        cout << "List is empty" << endl;
+//    }
+//    else if (shortList.isFull())
+//    {
+//        cout << "List is full" << endl;
+//    }
+//    
+//    if (shortList.updateAtIndex(dataIn,3))
+//    {
+//        shortList.display();
+//        shortList.getSmallest(min);
+//        
+//        cout << endl << "capacity is " << shortList.getCapacity() << "\t numValues is "
+//        << shortList.getNumValues() << "\t smallest value is " << min << endl;
+//    }
+//    else
+//    {
+//        cout << "Unable to insert the value 5 at index 1";
+//    }
+//    
+//    if (shortList.isEmpty())
+//    {
+//        cout << "List is empty" << endl;
+//    }
+//    else if (shortList.isFull())
+//    {
+//        cout << "List is full" << endl;
+//    }
+//    
+//    if (shortList.update(dataIn))
+//    {
+//        shortList.display();
+//        shortList.getSmallest(min);
+//        
+//        cout << endl << "capacity is " << shortList.getCapacity() << "\t numValues is "
+//        << shortList.getNumValues() << "\t smallest value is " << min << endl;
+//    }
+//    else
+//    {
+//        cout << "Unable to insert the value 5 at index 1";
+//    }
+//    
+//    if (shortList.isEmpty())
+//    {
+//        cout << "List is empty" << endl;
+//    }
+//    else if (shortList.isFull())
+//    {
+//        cout << "List is full" << endl;
+//    }
 }
 
 //*****************************************************************************************************
